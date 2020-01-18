dad9a0a07fa64facd38c09415943452ca24aaba0
德德 <523997244@qq.com>
2020-01-18 16:23:13 +0800

    更新数据处理插件，增加可选参数

--- 数据处理插件.py
+++ 数据处理插件.py
@@ -7,10 +7,10 @@ import dateutil  # 识别日期格式
 import chardet
 import numpy as np
 
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import plotly.offline as po
+# import plotly.express as px
+# import plotly.graph_objects as go
+# from plotly.subplots import make_subplots
+# import plotly.offline as po
 
 
 def is_wrong():
@@ -32,7 +32,7 @@ def read_file(file_path):
                 data = f.read()  # 读取文件内容
                 file_encoding = chardet.detect(data).get('encoding')
                 f.close()
-                df = pd.read_csv(file_path, encoding=file_encoding)
+                df = pd.read_csv(file_path, encoding=file_encoding, engine='python')
                 return df
             except Exception as e:
                 print(e)
@@ -43,6 +43,7 @@ def read_file(file_path):
         return df
     else:
         print("{}不是excel或者csv文件".format(file_path))
+        is_wrong()
         return None
 
 
@@ -69,6 +70,7 @@ def in_all(data_path, fill_name=True):
         if not os.path.isdir(data_path):
             print('未找到此文件夹，请重新输入')
             data_path = input('文件夹路径：').strip()
+            is_wrong()
         else:
             break
 
@@ -81,7 +83,7 @@ def in_all(data_path, fill_name=True):
                 df['fname'] = each.split('.')[0]
             row_df = pd.concat([row_df, df], sort=False)
         if each.endswith('csv'):
-            df = pd.read_csv(file)
+            df = read_file(file)
             if fill_name:
                 df['fname'] = each.split('.')[0]
             row_df = pd.concat([row_df, df], sort=False)
@@ -91,16 +93,15 @@ def in_all(data_path, fill_name=True):
     if not os.path.exists(out_path):
         os.makedirs(out_path)
 
-    # try:
     out_file = out_path + '/合并' + aft + '.xlsx'
-    writer = pd.ExcelWriter(out_file, engine='xlsxwriter', options={'strings_to_urls': False})
-    row_df.to_excel(writer)
-    writer.close()
+    df_to_excel(df=row_df, file_name=out_file)
+    # writer = pd.ExcelWriter(out_file, engine='xlsxwriter', options={'strings_to_urls': False})
+    # row_df.to_excel(writer)
+    # writer.close()
     print("文件合并保存在目录：{}".format(os.path.realpath(out_path)))
 
 
 def get_pic(file_path, height='140', width=''):
-
     """
     :param file_path: Excel文件路径
     :param key_words: 图片名称字段
@@ -114,12 +115,15 @@ def get_pic(file_path, height='140', width=''):
         if not os.path.exists(file_path):
             print('未找到该文件...', )
             file_path = input('请确认需要处理的文件路径：').strip()
+            is_wrong()
         else:
             break
     try:
         print(pd.read_excel(file_path).columns.to_list())
-    except:
+    except Exception as e:
+        print(e)
         print('Excel文件选择错误')
+        is_wrong()
     key_words = input('请在上述字段中选择，作为图片命名的字段(默认‘asin’,可直接回车)：').strip()
     if not key_words:
         key_words = 'asin'
@@ -136,16 +140,15 @@ def get_pic(file_path, height='140', width=''):
     print("图片辅助列已添加，在原Excel文件路径下查看")
 
 
-def daily_stock(stock_file, sale_file):
-
-    df_stock = pd.read_excel(stock_file)
-    df_sales = pd.read_excel(sale_file)
-    if "" not in df_stock.columns:
-        print('请确认库存数据包含字段：')
-        is_wrong()
-    if "" not in df_sales.columns:
-        print('请确认销量数据包含字段：')
-        is_wrong()
+# def daily_stock(stock_file, sale_file):
+#     df_stock = pd.read_excel(stock_file)
+#     df_sales = pd.read_excel(sale_file)
+#     if "" not in df_stock.columns:
+#         print('请确认库存数据包含字段：')
+#         is_wrong()
+#     if "" not in df_sales.columns:
+#         print('请确认销量数据包含字段：')
+#         is_wrong()
 
 
 def marketplace_choose(x):
@@ -159,6 +162,24 @@ def marketplace_choose(x):
 
 
 def get_deliver(sales_file, stock_file, on_the_way=6, mini_security_day=7, security_day=30):
+    print('如需默认参数，请直接回车')
+    # on_the_way = input('请输入在途运输时间(默认6天)：') or 6
+    # mini_security_day = input('输入安全库存天数：(默认7天)') or 7
+    # security_day = input('输入安全库存天数：(默认30天)') or 30
+    #
+    while True:
+        try:
+            on_the_way = int(input('请输入在途运输时间(默认6天)：') or 6)
+            mini_security_day = int(input('输入安全库存天数：(默认7天)') or 7)
+            security_day = int(input('输入安全库存天数：(默认30天)') or 30)
+
+        except Exception as e:
+            print(e)
+
+        print('在途时间{}，最小库存天数{}，安全库存天数为{}'.format(on_the_way,mini_security_day,security_day))
+        key_out = input('确认参数，直接回车或者输入q, 其他输入重新填写：',)
+        if not key_out or (key_out == 'q'):
+            break
 
     def get_stock_df(stock_file):
         df_stock = read_file(stock_file)
@@ -244,7 +265,7 @@ def get_deliver(sales_file, stock_file, on_the_way=6, mini_security_day=7, secur
 
         # 按照补货数量 对 补货状态进行调整
         df_last['应发状态'] = df_last['应发状态'].mask(pd.DataFrame([df_last['建议补货数量'].map(lambda x: 0 < x < 5),
-                                                df_last['最晚发货时间'] != -6]).all(), '考察')
+                                                             df_last['最晚发货时间'] != -6]).all(), '考察')
 
         return df_last
 
@@ -252,7 +273,9 @@ def get_deliver(sales_file, stock_file, on_the_way=6, mini_security_day=7, secur
     df_stock = get_stock_df(stock_file)
     df_last = get_last(df_stock_res=df_stock, df_sum_sale=df_sales)
     aft = datetime.datetime.now().strftime('_%Y-%m-%d-%H-%M') + "_发货计划.xlsx"
-    df_last.to_excel(stock_file.replace('.xlsx', aft))
+    file_name = stock_file.replace('.xlsx', aft).replace('.csv', aft)
+    df_to_excel(df=df_last, file_name=file_name)
+    print('发货计划存储位置：{}'.format(file_name))
 
 
 def get_daily(data_path):
@@ -271,14 +294,19 @@ def get_daily(data_path):
     for each in os.listdir(data_path):
         file = data_path + "/" + each
         if each.endswith('xlsx'):
-            df = pd.read_excel(file)
-            data_time = '2019' + each.split('.')[0]
-            data_time = dateutil.parser.parse(data_time).date()
+            df = read_file(file)
+            data_time = each.split('.')[0]
+            try:
+                data_time = dateutil.parser.parse(data_time).date()
+            except Exception as e:
+                print(e)
+                print('文件名并非可识别日期的格式，请更改文件名后重试')
+                is_wrong()
             df['date'] = data_time
             data_sum = pd.concat([data_sum, df], sort=False)
         if each.endswith('csv'):
-            df = pd.read_csv(file)
-            data_time = '2019' + each.split('.')[0]
+            df = read_file(file)
+            data_time = each.split('.')[0]
             data_time = dateutil.parser.parse(data_time).date()
             df['date'] = data_time
             data_sum = pd.concat([data_sum, df], sort=False)
@@ -289,14 +317,12 @@ def get_daily(data_path):
         if re.search('子.*ASIN', each, re.I):
             data_sum['sub_asin'] = data_sum[each]
 
-    # data_sum['asin'] = data_sum['(父） ASIN']
-    # data_sum['sub_asin'] = data_sum['（子）ASIN']
     data_info = data_sum[['date', 'asin', 'sub_asin', '买家访问次数', '订单商品数量转化率', '已订购商品数量', '已订购商品销售额']]
-    if 'date' in data_info.columns:
-        try:
-            data_info['date'] = data_info['date'].apply(lambda x: time.strftime('%Y-%m-%d', time.localtime(x / 1000)))
-        except:
-            pass
+    # if 'date' in data_info.columns:
+    #     try:
+    #         data_info['date'] = data_info['date'].apply(lambda x: time.strftime('%Y-%m-%d', time.localtime(x / 1000)))
+    #     except:
+    #         pass
     data_info.head()
 
     def get_num(strr):
@@ -327,72 +353,74 @@ def get_daily(data_path):
         os.makedirs(output_path)
     aft = datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d')
     df_to_excel(df=data_info, file_name=output_path + 'data_info_' + aft + '.xlsx')
+    print('文件保存在：{}'.format(output_path))
 
-def get_html(file):
-    file_path, file_name = os.path.split(os.path.realpath(file))
-    html_path = file_path + '/html/'
-    if not os.path.exists(html_path):
-        os.makedirs(html_path)
-
-    df_row = read_file(file)
-    asin_list = list(df_row['asin'].unique())
-    date_list = list(df_row['date'].unique())
-    try:
-        date_mouth = datetime.datetime.strftime(dateutil.parser.parse(date_list[0]), '%Y-%m')
-    except Exception as e:
-        print(e)
-        date_mouth = str(date_list[0])
-
-    df_asin = df_row.groupby(by=['asin', 'date']).agg(
-        {'买家访问次数': np.sum, '已订购商品数量': np.sum}).reset_index()
-    fig_asin = px.scatter(df_asin, x='date', y='asin', size='买家访问次数', color='已订购商品数量')
-
-    asin_count = len(asin_list)
-    col_num = 3
-    row_num = asin_count //3 + 1
-
-    fig_sub_asin = make_subplots(rows=row_num, cols=col_num)
-    for i in range(asin_count):
-        asin = asin_list[i]
-        df = df_row[df_row['asin'] == asin].groupby(by=['sub_asin', 'date']).agg(
-            {'买家访问次数': np.sum, '已订购商品数量': np.sum}).reset_index()
-        print(df.head())
-        fig_each = go.Scatter(x=df['sub_asin'],
-                              y=df['date'],
-                              # text=df['pic_url'],
-                              mode='markers',   # lines lines+markers
-                              marker=dict(
-                                  size=df['买家访问次数'],
-                                  color=df['已订购商品数量'],
-                                   ),
-                              text=df[['买家访问次数', '已订购商品数量']].to_dict('row'),
-                              name=asin,
-                              # hovertemplate="%{x}<br>%{y}<br>%{text}",
-                              hovertemplate=
-                              "%{x}<br>%{y}<br>%{text}"
-                              )
-        fig_sub_asin.add_trace(fig_each, i//3+1, i % 3+1)
-
-    fig_sub_asin.update_layout_images(dict(
-        # xref="paper",
-        # yref="paper",
-        sizex=0.1,
-        sizey=0.1,
-        xanchor="left",
-        yanchor="top"
-    ))
-    fig_sub_asin.update_layout(
-        showlegend=True,
-        title=date_mouth + '月度浏览量、销量比较',
-        width=600*col_num,
-        height=600*row_num,
-    )
-
-    aft = datetime.datetime.strftime(datetime.datetime.now(), '%H_%M')
-    asin_file_name = html_path + date_mouth + "_" + aft + '父ASIN_浏览量_销量汇总.html'
-    sub_asin_file_name = html_path + date_mouth + "_" + aft + '子ASIN_浏览量_销量汇总.html'
-    po.plot(fig_asin, filename=asin_file_name)
-    po.plot(fig_sub_asin, filename=sub_asin_file_name)
+#
+# def get_html(file):
+#     file_path, file_name = os.path.split(os.path.realpath(file))
+#     html_path = file_path + '/html/'
+#     if not os.path.exists(html_path):
+#         os.makedirs(html_path)
+#
+#     df_row = read_file(file)
+#     asin_list = list(df_row['asin'].unique())
+#     date_list = list(df_row['date'].unique())
+#     try:
+#         date_mouth = datetime.datetime.strftime(dateutil.parser.parse(date_list[0]), '%Y-%m')
+#     except Exception as e:
+#         print(e)
+#         date_mouth = str(date_list[0])
+#
+#     df_asin = df_row.groupby(by=['asin', 'date']).agg(
+#         {'买家访问次数': np.sum, '已订购商品数量': np.sum}).reset_index()
+#     fig_asin = px.scatter(df_asin, x='date', y='asin', size='买家访问次数', color='已订购商品数量')
+#
+#     asin_count = len(asin_list)
+#     col_num = 3
+#     row_num = asin_count // 3 + 1
+#
+#     fig_sub_asin = make_subplots(rows=row_num, cols=col_num)
+#     for i in range(asin_count):
+#         asin = asin_list[i]
+#         df = df_row[df_row['asin'] == asin].groupby(by=['sub_asin', 'date']).agg(
+#             {'买家访问次数': np.sum, '已订购商品数量': np.sum}).reset_index()
+#         print(df.head())
+#         fig_each = go.Scatter(x=df['sub_asin'],
+#                               y=df['date'],
+#                               # text=df['pic_url'],
+#                               mode='markers',  # lines lines+markers
+#                               marker=dict(
+#                                   size=df['买家访问次数'],
+#                                   color=df['已订购商品数量'],
+#                               ),
+#                               text=df[['买家访问次数', '已订购商品数量']].to_dict('row'),
+#                               name=asin,
+#                               # hovertemplate="%{x}<br>%{y}<br>%{text}",
+#                               hovertemplate=
+#                               "%{x}<br>%{y}<br>%{text}"
+#                               )
+#         fig_sub_asin.add_trace(fig_each, i // 3 + 1, i % 3 + 1)
+#
+#     fig_sub_asin.update_layout_images(dict(
+#         # xref="paper",
+#         # yref="paper",
+#         sizex=0.1,
+#         sizey=0.1,
+#         xanchor="left",
+#         yanchor="top"
+#     ))
+#     fig_sub_asin.update_layout(
+#         showlegend=True,
+#         title=date_mouth + '月度浏览量、销量比较',
+#         width=600 * col_num,
+#         height=600 * row_num,
+#     )
+#
+#     aft = datetime.datetime.strftime(datetime.datetime.now(), '%H_%M')
+#     asin_file_name = html_path + date_mouth + "_" + aft + '父ASIN_浏览量_销量汇总.html'
+#     sub_asin_file_name = html_path + date_mouth + "_" + aft + '子ASIN_浏览量_销量汇总.html'
+#     po.plot(fig_asin, filename=asin_file_name)
+#     po.plot(fig_sub_asin, filename=sub_asin_file_name)
 
 
 def main():
@@ -401,19 +429,19 @@ def main():
     :return:
     """
     print('请选择要使用的功能:')
-    print('-'*25)
-    for i in ['1: 表格合并', '2: 添加图片辅助列', '3: 生成发货表格']:
-        print('-'*8 + '{:-<20}'.format(i))
-    print('-'*25)
+    print('-' * 30)
+    for i in ['1: 表格合并', '2: 日常数据生成','3: 添加图片辅助列', '4: 生成发货表格']:
+        print('-' * 8 + '{:-<20}'.format(i))
+    print('-' * 30)
 
     while True:
         try:
-            mode = int(input('输入1-3对应功能的数字：'))
-            if mode in [1, 2, 3]:
+            mode = int(input('输入对应功能的数字：'))
+            if mode in [1, 2, 3, 4]:
                 break
         except Exception as e:
             print(e)
-            print('请确定输入1-3的数字')
+            print('请确定输入1-4的数字')
 
     while True:
 
@@ -429,12 +457,17 @@ def main():
             time.sleep(3)
 
         if mode == 2:
+            print('输入日常数据保存的文件夹路径：')
+            data_path = input('输入文件夹路径：').strip()
+            get_daily(data_path)
+
+        if mode == 3:
             print('输入excel文件路径，添加图片辅助列')
             file_path = input('输入Excel文件路径：').strip()
             get_pic(file_path)
             time.sleep(3)
 
-        if mode == 3:
+        if mode == 4:
             print('依据所传表格生成发货计划, 仅支持单站点操作，多站点会出现sku重复')
             sales_file = input('销量表格：').strip()
             stock_file = input('现有库存表格：').strip()
@@ -447,8 +480,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
-    # file_path = 'data_info2019-0418.xlsx'
-    # data_path = r'D:\Users\Administrator\Phe\陆\HA-子商品数据\HA-子商品数据'
-    data_path = 'D:\待合并'
-    in_all(data_path, fill_name=False)
+    main()
